0d66b69017d92e92e864dc426cb44b9255544d6e
cjh <529836078@qq.com>
2021-01-14 10:20:26 +0800

    处理网络请求可能引起的内存泄露问题

--- app/src/main/java/com/bajie/money/view/RemoteActivity.kt
+++ app/src/main/java/com/bajie/money/view/RemoteActivity.kt
@@ -5,10 +5,16 @@ import android.view.View
 import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
 import androidx.databinding.DataBindingUtil
+import androidx.lifecycle.Lifecycle
+import androidx.lifecycle.LifecycleOwner
 import androidx.lifecycle.ViewModelProvider
 import com.bajie.money.R
 import com.bajie.money.databinding.ActivityRemoteBinding
 import com.bajie.money.viewmodel.RemoteViewModel
+import com.uber.autodispose.AutoDispose
+import com.uber.autodispose.SingleSubscribeProxy
+import com.uber.autodispose.android.lifecycle.AndroidLifecycleScopeProvider
+import io.reactivex.Single
 
 /**
 
@@ -25,15 +31,21 @@ class RemoteActivity: AppCompatActivity() {
         model = ViewModelProvider(this).get(RemoteViewModel::class.java)
         mBinding.vm = model;
         mBinding.btn.setOnClickListener{
-            model.loadRemote().subscribe { _, error ->
-                error?.let {
-                    Toast.makeText(this, it.message, Toast.LENGTH_SHORT).show();
-                }
+            model.loadRemote()
+                .bindLifeCycle(this)
+                .subscribe { _, error ->
+                    error?.let {
+                        Toast.makeText(this, it.message, Toast.LENGTH_SHORT).show();
+                    }
             }
         }
 
 //
 
 //        mBinding.mask.setOnTouchListener(View.OnTouchListener { _, _ -> true });
+    }
+
+    fun  <T> Single<T>.bindLifeCycle(owner: LifecycleOwner): SingleSubscribeProxy<T> {
+        return this.`as`(AutoDispose.autoDisposable(AndroidLifecycleScopeProvider.from(owner, Lifecycle.Event.ON_DESTROY)))
     }
 }
